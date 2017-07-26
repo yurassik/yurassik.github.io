d1b3c86684c61fe17305e5cb91d6282aaf06c77c
yurassik <ypavlenko@voiptime.net>
2017-07-26 18:20:37 +0300

    Move files from public_html to root

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use('/', express.static('public_html'));
+app.use('/', express.static('./'));
 
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!');
